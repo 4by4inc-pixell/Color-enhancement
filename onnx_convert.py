9c71e428554ce2802fdd97efc59e3246c40624af
SunKyu <101241462+KwonseonKyu@users.noreply.github.com>
2025-07-11 16:16:42 +0900

    Update onnx_convert.py

--- onnx_convert.py
+++ onnx_convert.py
@@ -3,10 +3,16 @@ import torch.onnx
 from model import ColEn
 import os
 
-def convert_to_onnx(model_path, save_path="onnx_models/Color_Enhance.onnx", input_size=(1, 9, 256, 256)):
+def convert_to_onnx(
+    model_path, 
+    save_path="onnx_models/0710/Color_Enhance_0002_epoch0002.onnx", 
+    input_size=(1, 9, 256, 256),
+    filters=16
+):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = ColEn().to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    model = ColEn(filters=filters).to(device)
+    state_dict = torch.load(model_path, map_location=device, weights_only=True)
+    model.load_state_dict(state_dict, strict=True)
     model.eval()
 
     dummy_input = torch.randn(input_size).to(device)
@@ -29,4 +35,9 @@ def convert_to_onnx(model_path, save_path="onnx_models/Color_Enhance.onnx", inpu
     print(f"ONNX model saved to: {save_path}")
 
 if __name__ == "__main__":
-    convert_to_onnx(model_path="./saved_train_models/Color_Enhancement/ColorEnhance_epoch0221_valpsnr23.8786.pth")
+    convert_to_onnx(
+        model_path="./saved_train_models/Color_Enhancement_0710/ColorEnhance_epoch0002_valpsnr19.6860.pth",
+        save_path="onnx_models/0710/Color_Enhance_0002_epoch0002.onnx",
+        input_size=(1, 9, 256, 256),
+        filters=16
+    )
